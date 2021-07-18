49c8bed04e880f9cc5ad55edda1575d5d925758c
Anshul Mittal <me@anshulmittal.org>
2021-07-18 20:17:19 +0530

    updated for cleaner css

--- js/main.js
+++ js/main.js
@@ -1,9 +1,6 @@
 function load(e) {
     var file_name = e.getAttribute("data");
     $("#right").load(file_name);
-    // document.getElementById("right").innerHTML = '<object type="type/html" data="'+file_name+'" ></object>';
-    // document.getElementById("right").innerHTML="daara";
-    // console.log(document.getElementById("right").innerHTML = '<object type="type/html" data="' + file_name + '" ></object>');
 }
 
 function goto(e){
