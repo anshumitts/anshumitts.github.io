42ef2ce57ccfd7d67602ecd82942ffe86312f657
Anshul Mittal <me@anshulmittal.org>
2021-07-18 17:01:04 +0530

    updated css

--- js/main.js
+++ js/main.js
@@ -6,6 +6,12 @@ function load(e) {
     // console.log(document.getElementById("right").innerHTML = '<object type="type/html" data="' + file_name + '" ></object>');
 }
 
+function goto(e){
+    var url = e.getAttribute("url");
+    window.open(url, '_blank');
+}
+
 $(window).on('load', function () {
     $("#right").load('./html/aboutme.html');
+    $("#left").load('./html/nav.html');
 });
