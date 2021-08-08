b54abf5af02b89a61d1df7f0e547fac244834cf1
Anshul Mittal <me@anshulmittal.org>
2021-08-08 19:20:45 +0530

    updated for reload

--- js/main.js
+++ js/main.js
@@ -1,24 +1,4 @@
-function load(e) {
-    var file_name = e.getAttribute("data");
-    $("#right").load(file_name);
-}
-
-function goto(e){
-    var url = e.getAttribute("url");
-    window.open(url, '_blank');
-}
-
 $(window).on('load', function () {
     $("#right").load('./html/aboutme.html');
     $("#left").load('./html/nav.html');
-});
-
-$(document).ready(function(){
-    $('.in_nav_link').on('click',function(){
-        var aID = $(this).attr('href');
-        aID = aID.replace("#", "\.");
-        console.log(aID);
-        $("#right").load(aID);
-        return false
-    });
 });
